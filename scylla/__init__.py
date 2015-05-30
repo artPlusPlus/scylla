9f06faa74da711a57e5b8b785a301dc977c561cc
Matt Robinson <matt@technicalartisan.com>
2015-05-30 11:56:30 -0400

    Created package
    v0.3.1

--- scylla/__init__.py
+++ scylla/__init__.py
@@ -1,20 +1,26 @@
+# -*- coding: utf-8 -*-
 """
-Copyright (c) 2015 Contributors as noted in the AUTHORS file
+    scylla
+    ~~~~~
+    A framework for distributing dependency graphs.
+    :copyright: (c) 2015 by Matt Robinson.
+    :license:
+        Copyright (c) 2015 Contributors as noted in the AUTHORS file
 
-This file is part of scylla.
+        This file is part of scylla.
 
-scylla is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+        scylla is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Lesser General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
 
-scylla is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+        scylla is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
-along with scylla.  If not, see <http://www.gnu.org/licenses/>.
+        You should have received a copy of the GNU Lesser General Public License
+        along with scylla.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import zmq
---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+Scylla
+------
+
+Scylla is a framework for distributing dependency graphs.
+"""
+
+from setuptools import setup
+
+setup(
+    name='scylla',
+    version='0.3.1',
+    url='https://github.com/artPlusPlus/scylla',
+    license='LGPLv3',
+    author='Matt Robinson',
+    author_email='matt@technicalartisan.com',
+    description='A framework for distributing dependency graphs',
+    long_description=__doc__,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='zmq dependency graph',
+    packages=['scylla'],
+    install_requires=[
+        'pyzmq',
+
+    ],
+
+    extras_require={
+        'test': ['pytest'],
+    },
+)
